068a0d29079e4908a230bcc92df17094d8186de7
Faris714 <Farisalahmad714@gmail.com>
2024-10-16 19:23:55 -0700

    Initial Commit

---
+++ ProstheticScanner/ProstheticScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProstheticScannerApp.swift
+//  ProstheticScanner
+//
+//  Created by user270386 on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ProstheticScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProstheticScannerTests/ProstheticScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProstheticScannerTests.swift
+//  ProstheticScannerTests
+//
+//  Created by user270386 on 10/16/24.
+//
+
+import Testing
+@testable import ProstheticScanner
+
+struct ProstheticScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
